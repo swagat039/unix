81c4c951703e1a1f0efef8165b1238b8061a2396
swagat039 <75268379+swagat039@users.noreply.github.com>
2020-11-30 22:29:48 +0530

    Add files via upload

---
+++ guessinggame.sh
@@ -0,0 +1,22 @@
+#!/usr/bin/env bash
+
+function guess(){
+    true_ans=$(ls -l |grep "^-"|wc -l)
+    while true;
+    do
+        echo "Enter your guess:"
+        read  number
+        if [ $number -lt $true_ans ]
+        then
+            echo "Your guess is lower than the number of files!!"
+        elif [ $number -gt $true_ans ]
+        then
+            echo "Your guess is higher than the number of files!!"
+        else
+            echo "Congratulation, your guess is correct!!"
+        break;
+        fi
+    done
+}
+echo "Guess the number of files in the current directory:"
+guess
